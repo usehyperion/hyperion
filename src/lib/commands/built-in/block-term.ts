9f81b202778a20792d4ea738c67a9a2ff7631977
Oliver Rose <hello@olivermrose.com>
2026-02-14 18:22:04 -0500

    fix

--- src/lib/commands/built-in/block-term.ts
+++ src/lib/commands/built-in/block-term.ts
@@ -6,12 +6,13 @@ export default defineCommand({
 	provider: "Built-in",
 	name: "block-term",
 	description: "Block a term and prevent it from being used in chat",
+	modOnly: true,
 	args: ["term"],
 	async exec(args, channel) {
 		if (!args[0]) {
 			throw new CommandError(ErrorMessage.MISSING_ARG(this.args[0]));
 		}
 
-		await channel.blockTerm(args[0]);
+		await channel.blockTerm(args.join(" "));
 	},
 });
