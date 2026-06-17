138bbc1d61c1bcd7968f6b24240f01d61904708c
Oliver Rose <hello@olivermrose.com>
2026-06-17 15:42:40 -0400

    tooltip

--- src/lib/components/Emote.svelte
+++ src/lib/components/Emote.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { Emote } from "$lib/emotes";
 	import { settings } from "$lib/settings";
-	import * as Tooltip from "./ui/tooltip";
+	import Tooltip from "./ui/Tooltip.svelte";
 
 	interface Props {
 		emote: Emote;
@@ -13,44 +13,32 @@
 	const srcset = $derived(emote.srcset.join(", "));
 </script>
 
-<Tooltip.Root>
-	<Tooltip.Trigger>
-		{#snippet child({ props })}
-			<button
-				{...props}
-				class="-my-2 inline-grid align-middle"
-				type="button"
-				style:padding="{settings.state['chat.emotes.padding']}px"
-			>
-				<img
-					class="col-start-1 row-start-1 object-contain"
-					{srcset}
-					alt={emote.name}
-					decoding="async"
-				/>
+<button
+	class="-my-2 inline-grid align-middle"
+	type="button"
+	data-slot="tooltip-trigger"
+	style:padding="{settings.state['chat.emotes.padding']}px"
+>
+	<img
+		class="col-start-1 row-start-1 object-contain"
+		{srcset}
+		alt={emote.name}
+		decoding="async"
+	/>
 
-				{#each layers as layer}
-					<img
-						class="col-start-1 row-start-1 m-auto object-contain"
-						srcset={layer.srcset.join(", ")}
-						alt={layer.name}
-						decoding="async"
-					/>
-				{/each}
-			</button>
-		{/snippet}
-	</Tooltip.Trigger>
+	{#each layers as layer}
+		<img
+			class="col-start-1 row-start-1 m-auto object-contain"
+			srcset={layer.srcset.join(", ")}
+			alt={layer.name}
+			decoding="async"
+		/>
+	{/each}
+</button>
 
-	<Tooltip.Content side="top" sideOffset={4}>
-		<div class="flex flex-col items-center">
-			<img
-				{srcset}
-				alt={emote.name}
-				width={emote.width}
-				height={emote.height}
-				decoding="async"
-			/>
-			{emote.name}
-		</div>
-	</Tooltip.Content>
-</Tooltip.Root>
+<Tooltip side="top">
+	<div class="flex flex-col items-center">
+		<img {srcset} alt={emote.name} width={emote.width} height={emote.height} decoding="async" />
+		{emote.name}
+	</div>
+</Tooltip>
