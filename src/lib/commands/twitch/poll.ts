766af13205d5f475075030771c1f50d7af585aaa
Oliver Rose <hello@olivermrose.com>
2026-06-22 16:44:52 -0400

    fix

--- src/lib/commands/twitch/poll.ts
+++ src/lib/commands/twitch/poll.ts
@@ -1,4 +1,5 @@
 import { pollOpen } from "$lib/components/chat/PollDialog.svelte";
+import { CommandError } from "$lib/errors/command-error";
 import { defineCommand } from "../util";
 
 export default defineCommand({
@@ -6,7 +7,11 @@ export default defineCommand({
 	name: "poll",
 	description: "Create a poll for the channel",
 	modOnly: true,
-	async exec() {
+	async exec(_, channel) {
+		if (channel.poll?.status === "ACTIVE") {
+			throw new CommandError("A poll is already active.");
+		}
+
 		pollOpen.value = true;
 	},
 });
